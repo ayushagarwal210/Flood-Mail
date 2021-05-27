7f0138d0081dc1df56f38becfcb5a4af839e3898
Ayush <ayush.agarwal.ggn@gmail.com>
2021-05-27 22:00:05 +0530

    Added tinymce

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.conf import settings
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 # Create your views here.
 
 
@@ -12,7 +13,10 @@ def home(request):
         message = request.POST.get('message')
         email_from = settings.EMAIL_HOST_USER
         recepient_list = [email]
-        send_mail(subject, message, email_from, recepient_list)
+        html_message = render_to_string(
+            'email.html', {'message': message})
+        send_mail(subject, message, email_from,
+                  recepient_list, html_message=html_message)
         return redirect('/sent')
     return render(request, 'home.html')
 
